538b61a3b20173e2f48fad790cfa5d736a93c0f8
Kethankrk <kethankrk@gmail.com>
2024-09-13 19:05:42 +0530

    feat: cretead custom user with manager

--- api/models.py
+++ api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, username, password=None, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, username):
+        user = self.create_user(email=email, password=password, username=username)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class User(AbstractUser):
+    profile = models.URLField(blank=True, null=True)
+    first_name = None
+    last_name = None
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128, blank=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "password"]
+    object = UserManager()
