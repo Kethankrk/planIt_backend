0a39c43a015f9a261f7d37636cd4696be4a0a7b5
Kethankrk <kethankrk@gmail.com>
2024-09-17 18:40:30 +0530

    feat: added user not verified error

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -5,3 +5,9 @@ class UnautherizedError(ValidationError):
     def __init__(self, detail="Something went wrong", status_code=403):
         self.detail = detail
         self.status_code = status_code
+
+
+class UserNotVerifiedError(ValidationError):
+    def __init__(self, detail="User email not verified", status_code=401):
+        self.detail = detail
+        self.status_code = status_code
