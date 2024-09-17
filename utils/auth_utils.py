c2f3ccf84aa0d6ec859ac1f0b864c49ab971471e
Kethankrk <kethankrk@gmail.com>
2024-09-17 20:29:07 +0530

    refactor: removed some unwanted code (my mistake)

--- utils/auth_utils.py
+++ utils/auth_utils.py
@@ -8,8 +8,7 @@ from .exceptions import UnautherizedError, UserNotVerifiedError
 
 class CustomPermission(BasePermission):
     def authenticate(self, request):
-        payload = JWT_utils.is_jwt_authenticated(request=request)
-        JWT_utils.is_user_verified(payload=payload)
+        JWT_utils.is_jwt_authenticated(request=request)
 
 
 class JWT_utils:
@@ -37,16 +36,9 @@ class JWT_utils:
                 raise UnautherizedError("Invalid token header")
             token = token.split()[1]
             payload = JWT_utils.decode_token(token)
-            return payload
         except IndexError as e:
             raise UnautherizedError("Empty token found")
         except jwt.exceptions.InvalidSignatureError as e:
             raise UnautherizedError("Invalid token")
         except Exception as e:
             raise UnautherizedError(str(e))
-
-    @staticmethod
-    def is_user_verified(payload: dict) -> bool:
-        is_verified = payload.get("is_verified")
-        if not is_verified:
-            raise UserNotVerifiedError()
