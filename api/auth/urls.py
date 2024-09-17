9f8a2ee4dc38abef8bfe2bb7f1e6da62aca99edf
Kethankrk <kethankrk@gmail.com>
2024-09-17 21:13:31 +0530

    feat: added email verification urls

--- api/auth/urls.py
+++ api/auth/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .view import SignUpAPI, LoginAPI
+from .view import SignUpAPI, LoginAPI, EmailVerificationAPI
 
 urlpatterns = [
     path("signup/", SignUpAPI.as_view()),
     path("login/", LoginAPI.as_view()),
+    path("email-verify/", EmailVerificationAPI.as_view()),
 ]
