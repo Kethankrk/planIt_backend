a7da8940a370c51ec911a214571f1eef308913e6
Kethankrk <kethankrk@gmail.com>
2024-10-11 11:18:23 +0530

    feat: added google auth url

--- api/auth/urls.py
+++ api/auth/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .view import SignUpAPI, LoginAPI, EmailVerificationAPI
+from .view import SignUpAPI, LoginAPI, EmailVerificationAPI, GoogleAuthAPI
 
 urlpatterns = [
     path("signup/", SignUpAPI.as_view()),
+    path("google-auth/", GoogleAuthAPI.as_view()),
     path("login/", LoginAPI.as_view()),
     path("email-verify/", EmailVerificationAPI.as_view()),
 ]
