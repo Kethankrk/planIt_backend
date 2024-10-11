4792df9befe6c6641d5a2f5bc1a43006c89ca99e
Kethankrk <kethankrk@gmail.com>
2024-10-11 21:02:39 +0530

    feat: registered event model

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Event
 
 
-admin.site.register([User])
+admin.site.register([User, Event])
 # Register your models here.
