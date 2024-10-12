ac7af50e65fe6026590b1de6c9e548b2ba0cd5bd
Kethankrk <kethankrk@gmail.com>
2024-10-12 11:58:07 +0530

    feat: created urls for event form create and get

--- api/event/urls.py
+++ api/event/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .view import EventCreateGetListAPI
+from .view import EventCreateGetListAPI, EventFormCreateGetAPI
 
 
 urlpatterns = [
     path("", EventCreateGetListAPI.as_view()),
     path("<int:pk>/", EventCreateGetListAPI.as_view()),
+    path("form/", EventFormCreateGetAPI.as_view()),
+    path("form/<int:pk>/", EventFormCreateGetAPI.as_view()),
 ]
